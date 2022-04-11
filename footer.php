1e58d1e1dc642acd29c597b912072bd9a9ebe28f
Nikita Alexeenkov <78930989+lexeenkov@users.noreply.github.com>
2022-04-11 20:50:03 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>A simple HTML document</title>
+</head>
+<body>
+
+<p><strong>TWD sportcar</strong><br>
+      FMFI UK<br>
+			Mlynská dolina<br>
+			842 48 Bratislava<br>
+			tel: +421 2 123 456 <br>
+			e-mail: info@twdsportcar.sk</p>
+<h4>LINKS TO THE provided sources:</h4>
+    <p><a href="functions.php">functions</a> , <a href="header.php">header</a> , <a href="html-forms.html">forms</a> , <a href="navigation.php">navigation</a> , <a href="db.php">db</a></p>
+        </body>
+</html>
--- header.php
+++ header.php
@@ -6,10 +6,20 @@
 <link href="styles.css" rel="stylesheet">
 </head>
 <body>
-  <div id="gallery">
-  <img src="images/m_Maserati-GT-Stradale.jpg" alt="Maserati-GranTurismo-MC-Stradale" title="Maserati-GranTurismo-MC-Stradale" height="59" width="150"> <img src="images/m_Ferrari-458.jpg" alt="Ferrari-458-Italia" title="Ferrari-458-Italia" height="64" width="150"> <img src="images/m_Aston-Martin.jpg" alt="Aston-Martin-DBSCoupe" title="Aston-Martin-DBSCoupe" height="63" width="150"> <img src="images/m_Lamborghini-Gallardo.jpg" alt="Lamborghini-Gallardo-LP-570-4-Superleggera" title="Lamborghini-Gallardo-LP-570-4-Superleggera" height="66" width="150">
+<!---  <div id="gallery">
+  <img src="images/m_Maserati-GT-Stradale.jpg" alt="Maserati-GranTurismo-MC-Stradale" title="Maserati-GranTurismo-MC-Stradale" height="59" width="150"> 
   </div>
 <div id="main">
+-->
 	<header>
-		<h1><?php echo $headder_txt; ?></h1>
+        <div class="logo">
+            <a href=index.php><img src='images/logo.png' alt="PREMIUM MOTORSPORT"></a>
+            <ul class="nav">
+            <li><a href="cars.php"> CARS ></a></li>
+            <li><a href="signup.php"> SIGN UP></a></li>
+            <li><a href="login.php"> LOG IN ></a></li>
+            </ul>
+        </div>
+        
 	</header>
+    </body></html>
